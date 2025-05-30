93e98d90155d9335b038b18be3013869104b754d
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2025-05-30 03:36:02 -0300

    Add files via upload

--- allhackingtools.sh
+++ allhackingtools.sh
@@ -1,8 +1,75 @@
-#!/data/data/com.termux/files/usr/bin/bash
-apt update -y && apt upgrade -y
+#Upgrade All & Full Packages
+apt update
+apt upgrade
+
+#Add Your Termux Permision
 termux-setup-storage
+
+#Install Package Git
 apt install git -y
+
+#Install Package Python
+apt install python -y
+
+#Install Package Wget
+apt install wget -y
+
+#Install Package Curl
+apt install curl -y
+
+#Install Package Openssh
+apt install openssh -y
+
+#Install Package Php
+apt install php -y
+
+#Install Package Toilet
+apt install toilet -y
+
+#Install Package Colorama
+apt install colorama -y
+
+#Install Package Jq
+apt install jq -y
+
+#Install Package Apache2
+apt install apache2 -y
+
+#Install Package Ruby
+apt install ruby -y
+
+#Install Package Ssl
+apt install ssl -y
+
+#Install Package W3m
+apt install w3m -y
+
+#Install Package Figlet
+apt install figlet -y
+
+#Install Package Zip
+apt install zip -y
+
+#Install Package Lolcat
+apt install lolcat -y
+
+#Install Package Pv
+apt install pv -y
+
+#Install Package Neofetch
+apt install neofetch -y
+
+#Install Package Zsh
+apt install zsh -y
+
+#Install Package Ngrok
+apt install ngrok -y
+
+#Clone This Repository
 git clone https://github.com/mishakorzik/AllHackingTools
-cd scripts/AllHackingTools
-chmod +x Install.sh
+
+#Cd To Directory AllHackingTools
+cd /data/data/com.termux/files/scripts/AllHackingTools
+
+#Start Script To Install System
 bash Install.sh
